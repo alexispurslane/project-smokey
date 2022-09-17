533cea245dfa14b6d4e928bed83c104169fb816b
Alexis Dumas <alexispurslane@pm.me>
2022-09-17 18:01:07 -0400

    threads working (although it's one thread per job instead of two threads for the whole app

--- src/backend.rs
+++ src/backend.rs
@@ -1,10 +1,13 @@
+use std::{thread, time::Duration};
+
 use gtk::glib::random_double_range;
 
-use std::{thread, time::Duration};
+pub static DANGER_CUTOFF: f64 = 200.0;
+pub static WARNING_CUTOFF: f64 = 150.0;
 
 pub struct Prediction(pub f64);
 
 pub fn predict((lon, lat): (f64, f64)) -> Prediction {
     thread::sleep(Duration::from_secs(5));
-    Prediction(random_double_range(0.0, 1.0))
+    Prediction(random_double_range(100.0, 250.0))
 }
